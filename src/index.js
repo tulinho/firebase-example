9b7223c823e234b9f3b72df2995741a67b07aa26
Marco Tulio - DTI <dti123@localiza.com>
2017-06-02 00:54:20 -0300

    Layout development

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 require("./index.scss");
+
+const USER_NAME_COLORS = ['red-text', 'blue-text', 'pink-text', 'purple-text', 'indigo-text', 'green-text', 'orange-text', 'brown-text'];
+const USER_COLOR = USER_NAME_COLORS[Math.floor(Math.random() * 100) % USER_NAME_COLORS.length]; 
+
+$(document).ready(function(){
+
+	let messageTemplate = document.querySelector('#message-template');
+
+	$('#send-button').click(appendMessage);
+
+	function appendMessage(){
+		let textMessage = $('#message').val();
+		if(!!textMessage){
+			writeMessage('Anônimo', textMessage, USER_COLOR);
+			$('#message').val('');
+		}
+	}
+
+	function writeMessage(userName, message, userNameColor){
+		messageTemplate.content.querySelector('.name-span').className += (' ' + userNameColor);
+		messageTemplate.content.querySelector('.name-highlight').textContent = userName;
+		messageTemplate.content.querySelector('.message-span').textContent = message;
+		let clone = document.importNode(messageTemplate.content, true);
+		document.querySelector('#messages-container').appendChild(clone);
+	}
+
+});
