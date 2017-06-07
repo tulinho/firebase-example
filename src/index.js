ee6fa02f28c689cbd3302d85291cbe03c3a0a45e
Marco Tulio - DTI <dti123@localiza.com>
2017-06-06 22:30:17 -0300

    Add firebase and use firebase database

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ require("./index.scss");
 const USER_NAME_COLORS = ['red-text', 'blue-text', 'pink-text', 'purple-text', 'indigo-text', 'green-text', 'orange-text', 'brown-text'];
 const USER_COLOR = USER_NAME_COLORS[Math.floor(Math.random() * 100) % USER_NAME_COLORS.length]; 
 
+let config = {
+	apiKey: "AIzaSyA-Qa2wkNUcvEoh5D7YcwC-eDwnOEBC2PY",
+	authDomain: "mychatapp-7e450.firebaseapp.com",
+	databaseURL: "https://mychatapp-7e450.firebaseio.com",
+	projectId: "mychatapp-7e450",
+	storageBucket: "mychatapp-7e450.appspot.com",
+	messagingSenderId: "487649740643"
+};
+
 $(document).ready(function(){
+
+	firebase.initializeApp(config);
+	let auth = firebase.auth();
+	let database = firebase.database();
+	let storage = firebase.storage();
 
 	let messageTemplate = document.querySelector('#message-template');
 
@@ -12,13 +26,26 @@ $(document).ready(function(){
 	function appendMessage(){
 		let textMessage = $('#message').val();
 		if(!!textMessage){
-			writeMessage('Anônimo', textMessage, USER_COLOR);
+			database.ref('messages').push({
+				user: 'Anônimo',
+				text: textMessage,
+				color: USER_COLOR
+			});
 			$('#message').val('');
 		}
 	}
 
+	database.ref('/messages').on('child_added', onChildAdded);
+
+	function onChildAdded(messageSnapshot){
+		let message = messageSnapshot.val();
+		writeMessage(message.user, message.text, message.color);
+		let height = $('#messages-container')[0].scrollHeight;
+		$('#messages-container').scrollTop(height);
+	}
+
 	function writeMessage(userName, message, userNameColor){
-		messageTemplate.content.querySelector('.name-span').className += (' ' + userNameColor);
+		messageTemplate.content.querySelector('.name-span').className = ('name-span ' + userNameColor);
 		messageTemplate.content.querySelector('.name-highlight').textContent = userName;
 		messageTemplate.content.querySelector('.message-span').textContent = message;
 		let clone = document.importNode(messageTemplate.content, true);
