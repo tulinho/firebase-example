80a21f9d0b30c75671e6ade91570e00065a0b7ab
Marco Tulio - DTI <dti123@localiza.com>
2017-06-10 14:48:42 -0300

    webpack deploy

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,10 +1,9 @@
-module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
+var path = require('path');
+var config = {
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    path: 'target',
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.bundle.js'
   },
   module: {
     loaders: [
@@ -12,12 +11,10 @@ module.exports = {
       {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/octet-stream"},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,  loader: "file"},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=image/svg+xml"}
-    ]
-  },
-  devServer: {
-    inline: true,
-    contentBase: './src',
-    port: 3333
-}
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=image/svg+xml"},
+      {test: /\.js$/, exclude: ['node_modules'], loader: 'babel', preset: ['es2015']}
+      ]
+  }
 };
+
+module.exports = config;
